d05dcde2e45a89d90a770fad940a2610ee45dffe
unknown <peppardb@IT-4S03P22.corwinnet.city.cor.gov>
2015-11-23 15:55:05 -0600

    more changes

---
+++ test/actions/counter.spec.js
@@ -0,0 +1,47 @@
+import expect from 'expect'
+import { applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import * as actions from '../../actions/counter'
+
+const middlewares = [ thunk ]
+
+/*
+ * Creates a mock of Redux store with middleware.
+ */
+function mockStore(getState, expectedActions, onLastAction) {
+  if (!Array.isArray(expectedActions)) {
+    throw new Error('expectedActions should be an array of expected actions.')
+  }
+  if (typeof onLastAction !== 'undefined' && typeof onLastAction !== 'function') {
+    throw new Error('onLastAction should either be undefined or function.')
+  }
+
+  function mockStoreWithoutMiddleware() {
+    return {
+      getState() {
+        return typeof getState === 'function' ?
+          getState() :
+          getState
+      },
+
+      dispatch(action) {
+        const expectedAction = expectedActions.shift()
+        expect(action).toEqual(expectedAction)
+        if (onLastAction && !expectedActions.length) {
+          onLastAction()
+        }
+        return action
+      }
+    }
+  }
+
+  const mockStoreWithMiddleware = applyMiddleware(
+    ...middlewares
+  )(mockStoreWithoutMiddleware)
+
+  return mockStoreWithMiddleware()
+}
+
+describe('actions', () => {
+
+})
